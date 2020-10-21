95dd550e5357a5f92cf3504a9285c3de2164d211
George Mammen Kurien <72916722+chn19cs053@users.noreply.github.com>
2020-10-22 01:00:17 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-import java.math.*;
 
 public class Factorial {
 	public static void main (String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the value for 'n' to generate the fibonacci series: ");
 		int n = scan.nextInt();
+		scan.close();
 		int a = 0,b = 1, i = 1;
 		while(i <=n) {
 			System.out.print(a +" ");
